488ebc2509c182a97c297b7c381396af5ca1358f
rkim91 <richeekim@gmail.com>
2018-10-21 14:23:28 -0400

    update function

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const fetch = require("node-fetch");
 const url = "https://www.imdb.com/find?s=tt@ttype=ft&ref_=fn+ft&q=";
 
 function searchMovies(searchTerm) {
-  fetch(`${url}${searchTerm}`).then(res => res.text());
+  return fetch(`${url}${searchTerm}`).then(res => res.text());
 }
+
+searchMovies("star wars").then(body => {
+  console.log(body);
+});
